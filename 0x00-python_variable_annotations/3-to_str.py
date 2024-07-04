7b5ea827e74d44f885274f264102e7b766e8ac1b
birukshegute <birukshegute@gmail.com>
2024-07-04 22:29:55 +0000

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+takes a float n as argument and returns the string representation.
+"""
+
+
+def to_str(n: float)-> str:
+    """
+    returns thhe string representation of n.
+    """
+    return str(n)
