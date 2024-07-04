d0105395090fb29a1fc8b129bbbb3f9806a1de62
birukshegute <birukshegute@gmail.com>
2024-07-04 21:45:25 +0000

    task 0 attemp

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,6 +2,6 @@
 """ adds two integers and return a float"""
 
 
-def add(a: int, b: int)-> float:
+def add(a: float, b: float)-> float:
     """ all integers are annotated, and the return is float"""
     return a + b
