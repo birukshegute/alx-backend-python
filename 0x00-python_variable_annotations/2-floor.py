0cf2a1a7c9e03820bcfdb12174c6601c58100169
birukshegute <birukshegute@gmail.com>
2024-07-04 22:21:59 +0000

    task 2 edited

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-""" takes a float n as argument and returns the floor of the float """
+"""
+takes a float n as argument and returns the floor of the float
+"""
 
 
-def floor(n: float)-> float:
-    """ returns the floor of the floating number """
+def floor(n: float)-> int:
+    """
+    returns the floor of the floating number
+    """
 
-    return n//1
+    return int(n)
