bcd82849f4ea7e9a9169a154b00d43869aaddb8e
birukshegute <birukshegute@gmail.com>
2024-07-10 08:36:34 +0000

    I think I have fixed it - pycodestyle edition

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -8,7 +8,7 @@ import random
 from typing import List
 
 
-task_wait_random: float = __import__('3-tasks').task_wait_random
+task_wait_rand: float = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
@@ -16,7 +16,7 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     spawns  task_wait_random function n times
     """
 
-    tasks: List[asyncio.Task] = [(task_wait_random(max_delay)) for _ in range(n)]
+    tasks: List[asyncio.Task] = [(task_wait_rand(max_delay)) for _ in range(n)]
     delays: List[float] = [await task for task in asyncio.as_completed(tasks)]
 
     return delays
