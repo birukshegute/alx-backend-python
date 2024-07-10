51fc5cd88e1859d874c2976b3e667ac949fe9e4d
birukshegute <birukshegute@gmail.com>
2024-07-10 07:53:18 +0000

    it turns out I should have imported typing List

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,16 +5,17 @@ performs the task of the function wait_n
 
 import asyncio
 import random
+from typing import List
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
+wait_random: float = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     spawns  wait_random function n times
     """
 
-    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    delays = [await task for task in asynchio.as_completed(tasks)]
+    tasks: List[float] = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    delays: List[float] = [await task for task in asyncio.as_completed(tasks)]
 
     return delays
