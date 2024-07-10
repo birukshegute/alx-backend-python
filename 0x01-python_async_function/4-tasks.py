8911d3061cd77ce333c778702429156eff3c253d
birukshegute <birukshegute@gmail.com>
2024-07-10 08:34:42 +0000

    I think I have fixed it

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -13,13 +13,10 @@ task_wait_random: float = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    spawns  wait_random function n times
+    spawns  task_wait_random function n times
     """
 
-    tasks: List[float] = [
-            asyncio.create_task(task_wait_random(max_delay))
-            for _ in range(n)
-            ]
+    tasks: List[asyncio.Task] = [(task_wait_random(max_delay)) for _ in range(n)]
     delays: List[float] = [await task for task in asyncio.as_completed(tasks)]
 
     return delays
