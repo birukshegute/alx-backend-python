ea5f77d833e4b42635f9bab0c0b3c7d6396ff3ef
birukshegute <birukshegute@gmail.com>
2024-07-10 07:59:00 +0000

    pycodestyle violation, really hate annotating. It is making lines unnecessarily long for God's sake

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -10,12 +10,16 @@ from typing import List
 
 wait_random: float = __import__('0-basic_async_syntax').wait_random
 
+
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     spawns  wait_random function n times
     """
 
-    tasks: List[float] = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    tasks: List[float] = [
+            asyncio.create_task(wait_random(max_delay))
+            for _ in range(n)
+            ]
     delays: List[float] = [await task for task in asyncio.as_completed(tasks)]
 
     return delays
